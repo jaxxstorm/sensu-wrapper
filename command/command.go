7402251fb4dd6fef9f68a12ae8179ab0f402a5f2
Lee Briggs <lbriggs@apptio.com>
2017-01-10 12:27:22 +0000

    Move RunCommand into its own package

---
+++ command/command.go
@@ -0,0 +1,59 @@
+package command
+
+import (
+	"bytes"
+	"log"
+	"os/exec"
+	"syscall"
+	"time"
+)
+
+func RunCommand(cmdName string, cmdArgs []string, timeout int) (int, string) {
+
+	// the command we're going to run
+	cmd := exec.Command(cmdName, cmdArgs...)
+
+	// assign vars for output and stderr
+	var output bytes.Buffer
+	var stderr bytes.Buffer
+
+	// get the stdout and stderr and assign to pointers
+	cmd.Stderr = &stderr
+	cmd.Stdout = &output
+
+	// Start the command
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("Command not found: %s", cmdName)
+	}
+
+	timer := time.AfterFunc(time.Second*time.Duration(timeout), func() {
+		if timeout > 0 {
+			err := cmd.Process.Kill()
+			if err != nil {
+				panic(err)
+			}
+		}
+	})
+
+	// Here's the good stuff
+	if err := cmd.Wait(); err != nil {
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			// Command ! exit 0, capture it
+			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+				// Check it's nagios compliant
+				if status.ExitStatus() == 1 || status.ExitStatus() == 2 || status.ExitStatus() == 3 {
+					return status.ExitStatus(), stderr.String()
+				} else {
+					// If not, force an exit code 2
+					return 2, stderr.String()
+				}
+			}
+		} else {
+			log.Fatalf("cmd.Wait: %v", err)
+		}
+		timer.Stop()
+	}
+	// We didn't get captured, continue!
+	return 0, output.String()
+
+}
--- sensu-wrapper.go
+++ sensu-wrapper.go
@@ -1,68 +1,15 @@
 package main
 
 import (
-	"bytes"
+	"./command"
 	"encoding/json"
 	"fmt"
 	"gopkg.in/urfave/cli.v1"
 	"io/ioutil"
-	"log"
 	"net"
 	"os"
-	"os/exec"
 	"strings"
-	"syscall"
-	"time"
 )
-
-func run_command(cmdName string, cmdArgs []string, timeout int) (int, string) {
-
-	// the command we're going to run
-	cmd := exec.Command(cmdName, cmdArgs...)
-
-	// assign vars for output and stderr
-	var output bytes.Buffer
-	var stderr bytes.Buffer
-
-	// get the stdout and stderr and assign to pointers
-	cmd.Stderr = &stderr
-	cmd.Stdout = &output
-
-	// Start the command
-	if err := cmd.Start(); err != nil {
-		log.Fatalf("Command not found: %s", cmdName)
-	}
-
-	timer := time.AfterFunc(time.Second*time.Duration(timeout), func() {
-		if timeout > 0 {
-			err := cmd.Process.Kill()
-			if err != nil {
-				panic(err)
-			}
-		}
-	})
-
-	// Here's the good stuff
-	if err := cmd.Wait(); err != nil {
-		if exiterr, ok := err.(*exec.ExitError); ok {
-			// Command ! exit 0, capture it
-			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
-				// Check it's nagios compliant
-				if status.ExitStatus() == 1 || status.ExitStatus() == 2 || status.ExitStatus() == 3 {
-					return status.ExitStatus(), stderr.String()
-				} else {
-					// If not, force an exit code 2
-					return 2, stderr.String()
-				}
-			}
-		} else {
-			log.Fatalf("cmd.Wait: %v", err)
-		}
-		timer.Stop()
-	}
-	// We didn't get captured, continue!
-	return 0, output.String()
-}
 
 func main() {
 
@@ -118,7 +65,7 @@ func main() {
 		}
 
 		// runs the command args
-		status, output := run_command(c.Args().First(), c.Args().Tail(), timeout)
+		status, output := command.RunCommand(c.Args().First(), c.Args().Tail(), timeout)
 
 		sensu_values := &Output{
 			Name:     c.String("name"),
